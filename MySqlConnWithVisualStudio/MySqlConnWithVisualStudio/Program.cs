0cf7d5638e1a3e5f479668622adc2df5558ece1c
kiran04121997 <bhosalekiran259@gmail.com>
2022-05-20 11:10:24 +0530

    [kiran_bhosale]MySqlConnection

---
+++ MySqlConnWithVisualStudio/MySqlConnWithVisualStudio/Program.cs
@@ -0,0 +1,37 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySqlConnWithVisualStudio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string server = "localhost";
+            string database = "kirandb1";
+            string username = "root";
+            string password = "kiran@992192";
+            string constring = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + username + ";" + "PASSWORD=" + password + ";";
+            MySqlConnection conn = new MySqlConnection(constring);
+            conn.Open();
+            string query = "select * from Customers";
+            MySqlCommand cmd = new MySqlCommand(query, conn);
+            MySqlDataReader reader = cmd.ExecuteReader();
+            while (reader.Read())
+            {
+                Console.WriteLine(reader["CustomerID"]);
+                Console.WriteLine(reader["CustomerName"]);
+                Console.WriteLine(reader["ContactName"]);
+                Console.WriteLine(reader["Address"]);
+                Console.WriteLine(reader["City"]);
+                Console.WriteLine(reader["PostalCode"]);
+                Console.WriteLine(reader["Price"]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
